40c426d1231899a7a58ca72af48d676f26738630
Young <luoyang@xwfintech.net>
2021-03-10 16:15:21 +0800

    chore: init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import { View } from '@tarojs/components'
+import { FC } from 'react'
+import './index.module.scss'
+
+const Index: FC = () => {
+  return (
+    <View></View>
+  )
+}
+
+export default Index
